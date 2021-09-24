94f29058cdfdb6e364db789515c19fad544aced7
Killian <mkillian.development@gmail.com>
2021-09-24 18:08:05 +0200

    update app file

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,45 @@
 const express = require('express')
 const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+const userRoutes = require('./routes/users')
+const postsRoutes = require('./routes/posts')
 
 const app = express()
+
+app.use(bodyParser.json())
 app.use(helmet())
+app.use(cors())
+
+const { sequelize } = require('./config/sequelize')
+
+const dbTest = async function () {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+dbTest()
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
+
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postsRoutes)
 
 module.exports = app
